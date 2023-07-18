1740786b22c997a4428df5dd477f8a1b7791f6bf
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-07-18 09:56:14 -0300

    Instalando dependências

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    </>
+  )
+}
+
+
---
+++ src/router.jsx
@@ -0,0 +1,3 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter();
