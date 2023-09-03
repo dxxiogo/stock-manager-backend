36ffd466b66d622aa7fd6d25d56e114e30ee3b2d
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-03 18:37:08 -0300

    feat: creating update product controller

---
+++ backend/src/server/controllers/ProductsControllers/UpdateProductController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { prismaClient } from "../../database/prismaClient";
+
+
+
+async function updateProductController (req: Request, res: Response) {
+
+    const id = req.params.id;
+    const {name, inStock, category, price} = req.body
+
+    try {
+        const product = await prismaClient.product.update({
+            where: {
+                id
+            },
+            data: {
+                name,
+                category,
+                price,
+                inStock
+            }
+        })
+        res.status(200).json(product);
+
+    } catch (err) {
+        res.send({err})
+    }
+
+}
+
+export {updateProductController};
