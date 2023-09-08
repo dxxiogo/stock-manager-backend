ad7fb591e19c5cb857d2cbf33e5845c99783cc1f
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-08 16:37:19 -0300

    feat: add cors package for frontend integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import { server } from "./server/Server";
 import { router } from "./server/routes";
+import { Request, Response, NextFunction } from "express";
+import cors from 'cors';
 import 'dotenv/config'
 
 
@@ -9,4 +11,17 @@ server.listen(process.env.PORT || 5000, () => {
 })
 
 
+server.use(cors());
+
 server.use(router);
+
+// server.use((req: Request, res: Response, next: NextFunction) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173/');
+
+//     res.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+//     // Outros cabeçalhos e métodos CORS podem ser configurados aqui, se necessário
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+//     server.use(cors());
+//     next();
+// });
