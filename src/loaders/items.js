8bab9fe7035105adf7eb0cb83d3c12d045cc32e1
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-09-08 18:28:51 -0300

    feat: adding specific item request

--- src/loaders/items.js
+++ src/loaders/items.js
@@ -1,7 +1,10 @@
-export default function itemLoader ({params}) {
-    const items = JSON.parse(localStorage.getItem("items"));
-
-    const item = items.find(({id}) => id === +params.itemId);
+export default async function itemLoader ({params}) {
     
-    return item;
+    try {
+        const response = await fetch(`http://localhost:3333/product/${params.itemId}`);
+        const item = await response.json();
+        return item;
+    } catch(err) {
+        console.log(err)
+    }
 }
